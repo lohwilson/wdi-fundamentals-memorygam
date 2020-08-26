2c57583b34673a1a75c2da894e8cf62223c7b98f
Wil Son <lohwilson@hotmail.com>
2020-08-26 23:22:53 +0800

    added logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,1 +1,9 @@
 console.log("Up and running!");
+
+let cardOne = "queen";
+let cardTwo = "king";
+let cardthree = "queen";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
