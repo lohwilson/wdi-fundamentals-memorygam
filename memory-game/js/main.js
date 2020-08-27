4af41838abf0685d92f0ca745394aa953e499fca
Wil Son <lohwilson@hotmail.com>
2020-08-27 23:56:39 +0800

    added new js if else functions with alert

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,29 @@
-console.log("Up and running!");
+// console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "king";
-let cardthree = "queen";
-let cardFour = "king";
+// let cardOne = "queen";
+// let cardTwo = "king";
+// let cardthree = "queen";
+// let cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardTwo);
+
+let cards = ['queen', 'king', 'queen', 'king'];
+
+let cardsInPlay =[];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
+
+
